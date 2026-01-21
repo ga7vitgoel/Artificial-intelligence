0dd567fe16e0e7df6808336c6ab19ea9e557a7b8
ga7vitgoel <ggoel10911@gmail.com>
2026-01-21 17:57:49 +0400

    Add files via upload
    This project signals noise reduction. By applying a spatial filter, the script removes "pixel artifacts" to help AI models focus on significant features.
    
    The Code: ' cv.medianBlur(img, 5) '
    
    Syntax & Parameters: 
    -The parameter 5 defines the "kernel size" (a 5 *5 neighborhood of pixels). 
    -The central pixel is replaced by the median value of its neighbors.
    
    -Significance: Demonstrates the ability to perform data cleaning, ensuring that noise doesn't interfere with higher-level machine learning predictions.

---
+++ Smoothing an image.py
@@ -0,0 +1,27 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+##img = cv.imread('opencv-logo.png')
+
+img = cv.imread('ellipses.jpg')
+
+##blur = cv.blur(img,(5,5))
+
+##gauss_blur = cv.GaussianBlur(img,(5,5),0)
+
+median = cv.medianBlur(img,5)
+
+plt.subplot(121)
+plt.imshow(img)
+plt.title('Original')
+plt.xticks([])
+plt.yticks([])
+
+plt.subplot(122)
+plt.imshow(median)
+plt.title('Median Blur')
+plt.xticks([])
+plt.yticks([])
+
+plt.show()
